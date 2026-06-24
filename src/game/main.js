51d2c8807f5d4cfdbc86d8676cc925379068d954
NECK Vincent INNOV/IT-S <RBRM6214@orange.com>
2026-06-24 15:33:13 +0200

    v3.1 responsive

--- src/game/main.js
+++ src/game/main.js
@@ -8,7 +8,7 @@ import { MainMenu } from './scenes/MainMenu';
 import { LocalSetup } from './scenes/LocalSetup';
 import { RulesPanel } from './scenes/RulesPanel';
 import { OptionsPanel } from './scenes/OptionsPanel';
-import { AUTO, Game as PhaserGame } from 'phaser';
+import { AUTO, Game as PhaserGame, Scale } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
 // Find out more information about the Game Config at:
@@ -19,6 +19,12 @@ const config = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH,
+        width: 1024,
+        height: 768
+    },
     physics: {
         default: 'arcade',
         arcade: {
